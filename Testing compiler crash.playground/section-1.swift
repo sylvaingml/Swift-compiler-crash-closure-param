68b6cccd03954f19ada0865e1c573b4b52f8e78c
Sylvain Gamel <sylvain@SGinTech.fr>
2015-02-08 18:40:12 +0100

    Added a simple playground to reproduce issue.

---
+++ Testing compiler crash.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - Experiment a simple crash
+
+import Foundation
+import AppKit
+
+class Counter
+{
+    enum Sections: Int {
+        case counters = 0, action
+    }
+    
+    
+    var value: Int = 0
+    
+    
+    internal func changeCounter(
+        atIndexPath indexPath: NSIndexPath,
+        withUnary operation: ( Int ) -> Int
+        )
+    {
+        let section = Sections(rawValue: indexPath.indexAtPosition(0))
+        if ( .counters == section )
+        {
+            value = operation(value)
+        }
+    }
+    
+}
